7238b49ba1ef485fd0f6ee5e06d342f90dc188c6
Francesco <delta873@gmail.com>
2021-04-06 23:04:20 +0200

    nuovo commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <BasicComponent v-for="(name, i) in usernames" :name="name" :key="i"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
+ 
 </template>
 
 <script>
+import BasicComponent from './components/BasicComponent.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    BasicComponent
+  },
+
+  data: function(){
+    return{
+      usernames: ['pippo', 'pluto', 'paperino']
+    }
+    
   }
 }
 </script>
---
+++ src/components/BasicComponent.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+      <h2>Ciao {{name}}</h2>
+   </div>
+</template>
+
+
+<script>
+export default{
+      name: 'BasicComponents',
+   props: {
+      name: String
+   }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
